f15ca5c4aa016872faf62e61cd3c09e6a42c1b50
Cole Flournoy <flournoy.cole@gmail.com>
2020-12-15 23:52:29 -0500

    commit tree 5d13f8e536cdb24cd6a09755029687434c47697a

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,36 @@
+require "pry"
 
+class CashRegister
+    attr_accessor :total, :items, :previous_total
+    attr_reader :discount 
+    
+    def initialize(employee_discount=0)
+        @total = 0
+        @discount = employee_discount
+        @items = []
+    end
+    
+    def add_item(title, price, quantity=1)
+        @previous_total = @total
+        @total += price * quantity
+        quantity.times {@items << title}
+    end
+
+    def apply_discount
+        if self.discount > 0
+            reduction = self.total * (self.discount/100.to_f)
+            self.total -= reduction.to_i
+            "After the discount, the total comes to $#{self.total}."
+        else
+            "There is no discount to apply." 
+        end   
+    end
+
+    def void_last_transaction
+        if self.items == []
+            @total = 0
+        else   
+            @total = @previous_total
+        end
+    end
+end
